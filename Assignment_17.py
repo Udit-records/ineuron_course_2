0f318a2413e17fa0a564406a32470d5d1096b02c
Udit-records <111231744+Udit-records@users.noreply.github.com>
2022-10-05 02:00:48 +0530

    Add files via upload
    Kindly uncomment the section/objective before executing it

---
+++ Assignment_17.py
@@ -0,0 +1,80 @@
+"""
+print("Assignment_1")
+set_1 = {"Python", "C++", "C", "MATLAB", "OCTAVE", "JAVA"}
+print(set_1)
+print(type(set_1))
+"""
+
+
+"""
+print("Assignment_2")
+set_2 = {"Udit", 28, "Male", "Research_Scholar"}
+print(set_2)
+print(type(set_2))
+"""
+
+
+"""
+print("Assignment_3")
+set_3 = {"Python", "C++", "C", "MATLAB", "OCTAVE", "JAVA"}
+print("Finding datatype of the set \n")
+print(type(set_3))
+"""
+
+
+"""
+print("Assignment_4")
+thisset = {"Java","Python", "Django"}
+print("Python" in thisset)
+"""
+
+
+"""
+print("Assignment_5")
+thisset = {"Java", "Python", "SQL"}
+secondset= {"C", "Cpp", "NoSQL"}
+thisset = thisset.union(secondset)
+print(thisset)
+"""
+
+
+
+"""
+print("Assignment_6")
+thisset = {"Python", "Django", "JavaScript"}
+mylist = ["Java", "C"]
+#print({x for x in mylist})
+thisset = thisset.union({x for x in mylist})
+print(thisset)
+"""
+
+"""
+print("Assignment_7")
+thisset = {"Python", "Django", "JavaScript", "SQL"}
+#thisset.remove("SQL")
+#thisset.discard("SQL")
+thisset.pop()
+print(thisset)
+"""
+
+"""
+print("Assignment_8")
+thisset = {"Python", "Django", "JavaScript", "SQL"}
+thisset.clear()
+print(thisset)
+"""
+
+
+"""
+print("Assignment_9")
+thisset = {"Python", "Django", "JavaScript", "SQL"}
+{print(x) for x in thisset}
+"""
+
+
+"""
+print("Assignment_10")
+thisset = {0,1,2,3,4,5,6,7,8,9}
+print("The maximum value in the set is: ",max(thisset))
+print("The minimum value in the set is: ",min(thisset))
+"""
